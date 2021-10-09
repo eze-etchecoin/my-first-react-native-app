11e276838195df122adb5a01c87c517fba2bbad7
eze-etchecoin <eze.etchecoin.92@gmail.com>
2021-10-09 01:30:03 -0300

    modificando app.js

--- App.js
+++ App.js
@@ -1,21 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet, Image, TouchableOpacity, Alert } from "react-native"
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={style.container}>
+      <Text style={style.holaMundo}>Hello world!</Text>
+      <Image
+        source={{uri: 'https://picsum.photos/200/200' }}
+        style={style.image}
+      />
+      <TouchableOpacity onPress={() => Alert.alert("holi")} style={style.button}>
+        <Text style={style.textButton}>Press me</Text>
+      </TouchableOpacity>
     </View>
-  );
-}
+  )
+};
 
-const styles = StyleSheet.create({
+const style = StyleSheet.create({
   container: {
+    padding: 24,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#14A751'
   },
-});
+
+  holaMundo: {
+    marginTop: 24,
+    fontSize: 30,
+    color: '#fff'
+  },
+
+  image: {
+    height: 200,
+    width: 200
+  },
+
+  button: {
+    backgroundColor: '#000',
+    borderRadius: 5,
+    padding: 5,
+    margin: 5
+  },
+
+  textButton: {
+    color: '#fff',
+    fontSize: 20
+  }
+})
+
+export default App;
